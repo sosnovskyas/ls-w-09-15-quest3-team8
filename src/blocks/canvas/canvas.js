f0986204bc5d237b0e7db3d5660989a784bc0e8b
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-11-11 18:04:09 +0300

    maked file canvas.js added iief

--- src/blocks/canvas/canvas.js
+++ src/blocks/canvas/canvas.js
@@ -1,3 +1,17 @@
 ;(function() {
+  var canvas = document.getElementById("canvas"),
+    ctx       = canvas.getContext('2d'), // Контекст
+    canvasImage = new Image(),
+    watermarkImage = new Image();              // "Создаём" изображение
+  canvasImage.src = 'http://placeimg.com/600/400/1';
+  //watermarkImage.src = 'http://placeimg.com/200/200';
+
+  canvasImage.onload = function() {
+    ctx.drawImage(canvasImage, 100, 100,60,40);
+    watermarkImage.onload = function() {
+      //ctx.drawImage(watermarkImage, 10, 10);
+    };
+  };
+
 
 })();
