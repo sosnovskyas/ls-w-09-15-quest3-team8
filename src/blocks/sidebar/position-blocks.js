379fda29b940e0276c369513c8ee7116902179ec
thesiv95 <091995_mail@mail.ru>
2015-11-11 23:30:54 +0600

    top ok

--- src/blocks/sidebar/position-blocks.js
+++ src/blocks/sidebar/position-blocks.js
@@ -1,23 +1,38 @@
 (function () {
-    $('.position-blocks__link').on('click', function(e){
-        e.preventDefault();
-        var position = $(this).data('position'),
-            canvas_watermark = $('#canvas_watermark'),
-            canvas = canvas_watermark.parent('#canvas'),
-            canvas_hor = canvas.width(),
-            canvas_ver = canvas.height(),
-            h_size = Math.floor(canvas_hor / 3),
-            v_size = Math.floor(canvas_ver / 3);
-        // Первое время будет порядочно консоль-логов, для проверки. Потом уберу!
-        console.log(position + ' ' + canvas);
-        console.log(h_size);
-        console.log(v_size);
-        switch (position) {
-            case 'top-left':
-                canvas_watermark.css({
-                    'top': 0,
-                    'left': 0
-                });
-        }
-    });
+$('.position-blocks__link').on('click', function(e){
+  e.preventDefault();
+  var position = $(this).data('position'),
+  canvas_watermark = $('#canvas_watermark'),
+  canvas = canvas_watermark.parent('#canvas'),
+  canvas_hor = canvas.width(),
+  canvas_ver = canvas.height(),
+  h_size = Math.floor(canvas_hor / 3),
+  v_size = Math.floor(canvas_ver / 3);
+  // Первое время будет порядочно консоль-логов, для проверки. Потом уберу!
+  console.log(position + ' ' + canvas);
+  console.log(h_size);
+  console.log(v_size);
+  var top_right = h_size + 234; // пока не понимаю как можно запрограммировать расчет 234
+  console.log(top_right);
+  switch (position) {
+  case 'top-left':
+  canvas_watermark.css({
+  'top': 0,
+  'left': 0
+  }); break;
+  case 'top-center':
+
+  canvas_watermark.css({
+  'top': 0,
+  'left': h_size + 25
+  }); break;
+  case 'top-right':
+  canvas_watermark.css({
+  'top': 0,
+  'left': top_right
+  }); break;
+  }
+
+
+});
 }());
