5b34f703e4f2e2cfb33aeafcbfdef83da6c2566f
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-11-11 21:13:00 +0300

    canvas experement

--- src/blocks/canvas/canvas.js
+++ src/blocks/canvas/canvas.js
@@ -1,17 +1,24 @@
-;(function() {
+;(function(window) {
   var canvas = document.getElementById("canvas"),
     ctx       = canvas.getContext('2d'), // Контекст
     canvasImage = new Image(),
     watermarkImage = new Image();              // "Создаём" изображение
-  canvasImage.src = 'http://placeimg.com/600/400/1';
-  //watermarkImage.src = 'http://placeimg.com/200/200';
+
+    canvasImage.src = 'http://placeimg.com/600/400/1';
+    watermarkImage.src = 'http://placeimg.com/200/200/1';
 
   canvasImage.onload = function() {
-    ctx.drawImage(canvasImage, 100, 100,60,40);
-    watermarkImage.onload = function() {
-      //ctx.drawImage(watermarkImage, 10, 10);
-    };
+      try{
+        ctx.drawImage(canvasImage, 0, 0);
+        ctx.drawImage(watermarkImage, 10, 10);
+      } catch(e) {
+        alert('error');
+      }
   };
+  canvas.addEventListener("mousedown", mouseDownListener, false);
+  window.addEventListener("mouseup", mouseUpListener, false);
 
+  function mouseDownListener() {
 
-})();
+  }
+})(window);
