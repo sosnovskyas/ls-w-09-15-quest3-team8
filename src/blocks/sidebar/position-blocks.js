bce63254ad2fe9ac61789e63e2d003c688f120ec
thesiv95 <091995_mail@mail.ru>
2015-11-11 23:57:38 +0600

    left ok

--- src/blocks/sidebar/position-blocks.js
+++ src/blocks/sidebar/position-blocks.js
@@ -1,7 +1,8 @@
 (function () {
 $('.position-blocks__link').on('click', function(e){
   e.preventDefault();
-  var position = $(this).data('position'),
+    var $this = $(this),
+   position = $this.data('position'),
   canvas_watermark = $('#canvas_watermark'),
   canvas = canvas_watermark.parent('#canvas'),
   canvas_hor = canvas.width(),
@@ -15,22 +16,32 @@ $('.position-blocks__link').on('click', function(e){
   var top_right = h_size + 234; // пока не понимаю как можно запрограммировать расчет 234
   console.log(top_right);
   switch (position) {
-  case 'top-left':
-  canvas_watermark.css({
-  'top': 0,
-  'left': 0
-  }); break;
-  case 'top-center':
+    case 'top-left':
+    canvas_watermark.css({
+      'top': 0,
+      'left': 0
+    }); break;
+    case 'top-center':
+    canvas_watermark.css({
+      'top': 0,
+      'left': h_size + 25
+    }); break;
+    case 'top-right':
+    canvas_watermark.css({
+      'top': 0,
+      'left': top_right
+    }); break;
+    case 'middle-left':
+    canvas_watermark.css({
+      'top': v_size,
+      'left': 0
+    }); break;
+      case 'bottom-left':
+      canvas_watermark.css({
+          'top': v_size + 158,
+          'left': 0
+      }); break;
 
-  canvas_watermark.css({
-  'top': 0,
-  'left': h_size + 25
-  }); break;
-  case 'top-right':
-  canvas_watermark.css({
-  'top': 0,
-  'left': top_right
-  }); break;
   }
 
 
