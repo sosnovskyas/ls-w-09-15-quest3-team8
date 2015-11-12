de8b51173277fc90acc9939c8134d3f1a79c821a
PshlT <poshelya@gmail.com>
2015-11-12 13:48:54 +0300

    Opacity try

--- src/blocks/sidebar/opacity.js
+++ src/blocks/sidebar/opacity.js
@@ -1,1 +1,21 @@
 
+$(document).ready(function() {
+  $(function() {
+    $("#opacity__slider").slider({
+      orientation: "horizontal",
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 100,
+      slide: function(event, ui) {
+        var opacity_val = (ui.value) / 100;
+
+        $("#canvas_watermark").css({
+          opacity: opacity_val
+        })
+      }
+    });
+    $("#amount").val($("#slider").slider("value"));
+  });
+
+});
