cf91153472eea9c51c2e8200bbde350fe5a2dabb
Ivanov Sergey <poshelya@gmail.com>
2015-11-12 20:59:27 +0300

    opacity check 12.11.15

--- src/blocks/sidebar/opacity.js
+++ src/blocks/sidebar/opacity.js
@@ -1,1 +1,20 @@
+$(document).ready(function() {
+    $(function() {
+        $("#opacity-slider").slider({
+            orientation: "horizontal",
+            range: "min",
+            min: 0,
+            max: 100,
+            value: 100,
+            slide: function(event, ui) {
+                var opacity_val = (ui.value) / 100;
 
+                $("#canvas_watermark").css({
+                    opacity: opacity_val
+                })
+            }
+        });
+        $("#amount").val($("#slider").slider("value"));
+    });
+
+});
