eb56a324a29e2be8b351898349ccdfbd528afd7e
Ivanov Sergey <poshelya@gmail.com>
2015-11-12 22:01:45 +0300

    iief

--- src/blocks/sidebar/opacity.js
+++ src/blocks/sidebar/opacity.js
@@ -1,20 +1,22 @@
-$(document).ready(function() {
-  $(function() {
-    $("#opacity__slider").slider({
-      orientation: "horizontal",
-      range: "min",
-      min: 0,
-      max: 100,
-      value: 100,
-      slide: function(event, ui) {
-        var opacity_val = (ui.value) / 100;
+;(function() {
+  'use strict';
+  $(document).ready(function() {
+    $(function() {
+      $("#opacity-slider").slider({
+        orientation: "horizontal",
+        range: "min",
+        min: 0,
+        max: 100,
+        value: 100,
+        slide: function(event, ui) {
+          var opacity_val = (ui.value) / 100;
 
-        $("#canvas_watermark").css({
-          opacity: opacity_val
-        })
-      }
+          $("#canvas_watermark").css({
+            opacity: opacity_val
+          })
+        }
+      });
+      $("#amount").val($("#slider").slider("value"));
     });
-    $("#amount").val($("#slider").slider("value"));
   });
-
-});
+})();
