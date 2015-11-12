5583651828d6ecfe282036259bd337e85bc7fd23
thesiv95 <091995_mail@mail.ru>
2015-11-12 16:06:30 +0600

    spinner x,y

--- src/blocks/sidebar/position-blocks.js
+++ src/blocks/sidebar/position-blocks.js
@@ -1,36 +1,52 @@
 (function () {
 $('.position-blocks__link').on('click', function(e){
-  e.preventDefault();
-  var $this = $(this),
-    position = $this.data('position'),
-    canvas_watermark = $('#canvas_watermark'),
-    canvas = canvas_watermark.parent('#canvas'),
+  e.preventDefault(); // убираем стандартную реакцию браузера на ссылки
+  var $this = $(this), // ссылка по которой мы кликнули (то есть квадратик)
+    position = $this.data('position'), // получаем информацию о положении из разметки
+
+  // Для вывода координат в инпуты X и Y возьмем эти инпуты
+    spinner_x = $('#spinner_x'),
+    spinner_y = $('#spinner_y'),
+
+  // Поработаем с картинками
+    canvas_watermark = $('#canvas_watermark'), // возьмем вотермарк чтобы двигать его
+    canvas = canvas_watermark.parent('#canvas'), // возьмем сам канвас для расчета ширины и высоты
     canvas_hor = canvas.width(), // по горизонтали 650
     canvas_ver = canvas.height(), // по вертикали 535
     h_size = Math.floor(canvas_hor / 3), // 216
     v_size = Math.floor(canvas_ver / 3); // 178
-  // СДВИГАЕМ
+
+  // Дополнительные переменные для расчета координат
   var top_right = h_size + (h_size / 0.9), // 456
       centr = h_size + (h_size / 8.64), // 241
       bottm = v_size + (v_size / 1.126); // 336.0817051509769
 
-
+  // Правим CSS у вотермарка и вносим информацию в инпуты в зависимости от положения
   switch (position) {
-  case 'top-left':
+    case 'top-left':
     canvas_watermark.css({
     'top': 0,
     'left': 0
-  }); break;
+  });
+    spinner_x.val(0);
+    spinner_y.val(0);
+    break;
   case 'top-center':
     canvas_watermark.css({
     'top': 0,
     'left': centr
-  }); break;
+  });
+    spinner_x.val(centr); // = left
+    spinner_y.val(0); // = top
+    break;
   case 'top-right':
     canvas_watermark.css({
     'top': 0,
     'left': top_right
-  }); break;
+  });
+    spinner_x.val(top_right);
+    spinner_y.val(0);
+    break;
   case 'middle-left':
     canvas_watermark.css({
     'top': v_size,
@@ -65,4 +81,8 @@ $('.position-blocks__link').on('click', function(e){
 
 
 });
+
+
+
+
 }());
