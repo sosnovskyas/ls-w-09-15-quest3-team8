45f69315df55bb4ec978f946ead9745033998163
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-11-15 00:36:03 +0300

    included MATH

--- src/blocks/sidebar/file-upload.js
+++ src/blocks/sidebar/file-upload.js
@@ -1,11 +1,43 @@
 var canvasWidth = 650;
 var canvasHeight = 535;
+var canvasScale = 1;
+
+var backgroundWidth = 0;
+var backgroundHeight = 0;
+var canvasScale = 1;
+
+var watermarkWidth = 0;
+var watermarkHeight = 0;
+var watermarkScale = 1;
 
 var bgFile = new FileReader();
-var wmFile = new FileReader();
 var bgImage = new Image();
+
+var wmFile = new FileReader();
 var wmImage = new Image();
 
+// совсем немножко чудо математики ;)
+function returnScale(w1,h1,w2,h2){
+  var s1 = w2/h2;
+  var s2 = w1/h1;
+  var w = 0;
+  var h = 0;
+
+  if(s1 > s2){
+      w = w1;
+      h =(w1*h2/w2);
+  } else if(s1 < s2) {
+      w = (h1*w2/h2);
+      h = h1;
+  } else if(s1 == s2) {
+    w = w1;
+    h = h1;
+  }
+  return {
+    w: Math.floor(w),
+    h: Math.floor(h)
+  }
+}
 
 function getBgFile(input) {
   if (input.files && input.files[0]) {
@@ -15,14 +47,15 @@ function getBgFile(input) {
       $('.upload-image__fake-input').val(input.files[0].name);
 
       bgImage.onload = function() {
+        bg = returnScale(canvasWidth, canvasHeight, bgImage.width, bgImage.height);
+        backgroundWidth = bg.w;
+        backgroundHeight = bg.h;
+
         $('#canvas__img')
           .attr('src', e.target.result)
-          .width(bgImage.width)
-          .height(bgImage.height)
-          .css({
-            top:0,
-            left: 0
-          });
+          .width(backgroundWidth)
+          .height(backgroundHeight)
+        ;
       };
     };
     bgFile.readAsDataURL(input.files[0]);
@@ -37,10 +70,14 @@ function getWatermarkFile(input) {
       $('.upload-watermark__fake-input').val(input.files[0].name);
 
       wmImage.onload = function() {
+        wm = returnScale(backgroundWidth, backgroundHeight, wmImage.width, wmImage.height);
+        watermarkWidth = wm.w;
+        watermarkHeight = wm.h;
+
         $('#canvas__watermark')
           .attr('src', e.target.result)
-          .width(wmImage.width)
-          .height(wmImage.height)
+          .width(watermarkWidth)
+          .height(watermarkHeight)
           .css({
             top:0,
             left: 0
