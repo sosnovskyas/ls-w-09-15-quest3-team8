f33dde6b7250d54cbf30a31b2d9fda6f7547df14
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-11-15 15:28:09 +0300

    begin sending files

---
+++ src/blocks/sidebar/download.js
@@ -0,0 +1,29 @@
+var downloadResult = function(event) {
+
+  //event.preventDefault();
+  var submitButton = $(this).find('input[type="submit"]');
+  $(submitButton).attr('disabled', '');
+  var
+    form   = $(this),
+    url    = form.attr('action'),
+    data   = form.serialize(),
+    result = $.ajax({
+      url: url,
+      type: 'POST',
+      dataType: 'json',
+      data: data
+    })
+      .done(function() {
+        console.log("sucsess");
+        openSucsessPopup();
+        resetForm();
+      })
+      .fail(function() {
+        console.log("error");
+        openFailPopup();
+      })
+      .always(function() {
+        $(submitButton).removeAttr('disabled');
+      });
+  debugger;
+};
