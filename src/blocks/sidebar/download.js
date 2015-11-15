2e81680d524389d15902fc59678caf49bc0dfc3b
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-11-15 15:59:07 +0300

    added send ajax

--- src/blocks/sidebar/download.js
+++ src/blocks/sidebar/download.js
@@ -1,11 +1,29 @@
-var downloadResult = function(event) {
+
+$('.setting__form').on('submit', function(e) {
+  e.preventDefault();
+  console.log($(this).serialize())
+  var _data = $(this).serialize();
+  $.ajax({
+    type: 'POST',
+    url: 'watermark.php',
+    data: _data,
+    success: function(data) {
+      $('.results').html(data);
+    },
+    error:  function(xhr, str){
+      alert('Возникла ошибка: ' + xhr.responseCode);
+    }
+  });
+});
+
+/*
 
   //event.preventDefault();
   var submitButton = $(this).find('input[type="submit"]');
   $(submitButton).attr('disabled', '');
   var
     form   = $(this),
-    url    = form.attr('action'),
+    url    = 'watermark.php',
     data   = form.serialize(),
     result = $.ajax({
       url: url,
@@ -24,5 +42,4 @@ var downloadResult = function(event) {
       })
       .always(function() {
         $(submitButton).removeAttr('disabled');
-      });
-};
+      });*/
