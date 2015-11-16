7a319c2e4de910c1d4af9395e32e2ab962e82a8d
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-11-16 23:12:01 +0300

    refact

--- src/blocks/sidebar/download.js
+++ src/blocks/sidebar/download.js
@@ -1,10 +1,16 @@
-var canvas = document.getElementById('result');
-var context = canvas.getContext('2d');
 
 $('.setting__form')
   .attr('disabled', '')
   .on('submit', function(e) {
     e.preventDefault();
+
+    var rawCanva = document.createElement('canvas');
+    rawCanva.setAttribute('id','result');
+    document.getElementById('result-wrapper').appendChild(rawCanva);
+
+    var canvas = document.getElementById('result');
+    var context = canvas.getContext('2d');
+
     canvas.width = backgroundWidth;
     canvas.height = backgroundHeight;
     context.drawImage(bgImage, 0, 0, backgroundWidth, backgroundHeight);
@@ -14,4 +20,5 @@ $('.setting__form')
     context.globalAlpha= (opacityValue / 100);
     context.drawImage(wmImage, x, y, watermarkWidth, watermarkHeight);
     window.location.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    rawCanva.destroy();
   });
