fffb73253a0a04df6d952f82d6b2a3aeda06174a
Evgeniya Pronina <rezinkina@list.ru>
2015-11-16 20:32:55 +0600

    added backend files

--- src/backend/watermark.php
+++ src/backend/watermark.php
@@ -1,3 +1,30 @@
 <?php
-/**
- */
+require_once 'vendor/autoload.php';
+use WideImage\WideImage as WideImage;
+
+$background = WideImage::load($_POST['bgFile']);
+$watermark = WideImage::load($_POST['wmFile']);
+
+$wmParam = getimagesize($_POST['wmFile']); // Получаем размеры вотермарка в маcсив
+$wmWidth = $wmParam[0]; //  ширина
+$wmNewWidth = $wmWidth * $_POST['wmScale'];
+//изменяем размер в соотв с нашей областью экрана
+$resized = $watermark->resize($wmNewWidth);
+
+
+if ($_POST['positionX'] == '') {
+    $x = 0;
+}else{
+    $x = $_POST['positionX'];
+}
+
+if ($_POST['positionY'] == '') {
+    $y = 0;
+}else{
+    $y = $_POST['positionY'];
+}
+
+$background->merge($resized, $x, $y, $_POST['opacity'])->saveToFile('new.jpg');;
+
+//накладываем файлы, задаем прозрачность и выводим на экран
+$background->merge($resized, $x, $y, $_POST['opacity'])->output('jpg');
