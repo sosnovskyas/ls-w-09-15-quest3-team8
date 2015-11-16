afffe04fcd617746608f460c349717d9e29660d0
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-11-16 17:08:50 +0300

    spinner

--- src/blocks/sidebar/download.js
+++ src/blocks/sidebar/download.js
@@ -21,31 +21,3 @@ $('.setting__form')
       }
     });
   });
-
-/*
-
-  //event.preventDefault();
-  var submitButton = $(this).find('input[type="submit"]');
-  $(submitButton).attr('disabled', '');
-  var
-    form   = $(this),
-    url    = 'watermark.php',
-    data   = form.serialize(),
-    result = $.ajax({
-      url: url,
-      type: 'POST',
-      dataType: 'json',
-      data: data
-    })
-      .done(function() {
-        console.log("sucsess");
-        openSucsessPopup();
-        resetForm();
-      })
-      .fail(function() {
-        console.log("error");
-        openFailPopup();
-      })
-      .always(function() {
-        $(submitButton).removeAttr('disabled');
-      });*/
