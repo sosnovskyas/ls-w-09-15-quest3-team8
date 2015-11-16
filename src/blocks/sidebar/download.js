2e0933243a2b6a2ce384ccdce2d8e88d6c7ad7ae
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-11-16 22:58:53 +0300

    cosmetic

--- src/blocks/sidebar/download.js
+++ src/blocks/sidebar/download.js
@@ -5,21 +5,13 @@ $('.setting__form')
   .attr('disabled', '')
   .on('submit', function(e) {
     e.preventDefault();
-
     canvas.width = backgroundWidth;
     canvas.height = backgroundHeight;
-
     context.drawImage(bgImage, 0, 0, backgroundWidth, backgroundHeight);
-
-
     var x = document.getElementById('spinner_x').value;
     var y = document.getElementById('spinner_y').value;
     var opacityValue = document.getElementById('opacity__input').value;
-    //$('body').append('<canvas id=result width=' + backgroundWidth + ' height = '+backgroundHeight +' />');
     context.globalAlpha= (opacityValue / 100);
     context.drawImage(wmImage, x, y, watermarkWidth, watermarkHeight);
-
-    //canvas.toDataURL('image/jpeg');
-    //var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
     window.location.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
   });
