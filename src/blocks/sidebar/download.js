a2ec5c0abdcce6ace7bde543e1309605cbf0ac15
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-11-18 00:42:13 +0300

    opacity bug fix

--- src/blocks/sidebar/download.js
+++ src/blocks/sidebar/download.js
@@ -16,7 +16,12 @@ $('.setting__form')
     context.drawImage(bgImage, 0, 0, backgroundWidth, backgroundHeight);
     var x = document.getElementById('spinner_x').value;
     var y = document.getElementById('spinner_y').value;
-    var opacityValue = document.getElementById('opacity__input').value;
+    var opacityValue = 100.0;
+    var opacity = document.getElementById('opacity__input').value;
+    if (opacity){
+      opacityValue = opacity;
+    }
+    console.log(opacity);
     context.globalAlpha= (opacityValue / 100);
     context.drawImage(wmImage, x, y, watermarkWidth, watermarkHeight);
     window.location.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
